76dbf4ae6ece8286e9e32349d1e45d79a49f14c7
Sai Charan Nancharla <132137396+sai-charan-nancharla@users.noreply.github.com>
2024-07-29 18:06:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           <Link to='/about'>About</Link>
         </div>
         <div className="text-3xl sm:text-2xl font-extrabold">
-          <a href="/">Sai Charan Nancharla</a>
+          Sai Charan Nancharla
         </div>
         <div className="hidden sm:flex space-x-4 items-center text-sm">
           <Link to='/projects'>Projects</Link>
